184d1204aa08a15d1e9e2f3ad6ce547f66867cc6
Dhirtiman Khati <108656037+dhirtiman@users.noreply.github.com>
2024-03-16 19:26:27 +0530

    Revert "Som"

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,10 @@
 import { createStore } from "vuex";
 
 import studentAuthModule from './modules/student/studentAuth/index.js'
-import teacherAuthModule from "./modules/teacher/teacherAuth/index.js";
 
 const store = createStore({
     modules: {
         student: studentAuthModule,
-        teacher: teacherAuthModule,
     },
 });
 
